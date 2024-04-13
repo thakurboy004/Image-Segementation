4bbb9dd45b7d528f4d83fb6526b0631238e3eb64
Abhishek Thakur <75372728+thakurboy004@users.noreply.github.com>
2024-04-13 15:34:06 +0000

    Final chnages before deploying the project

--- app.py
+++ app.py
@@ -8,32 +8,31 @@ app = Flask(__name__)
 def first_project():
   return render_template("home.html")
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/predicting", methods=["GET", "POST"])
 def predicting():
   if request.method == "POST":
     input_data = [
-        request.form.get("REGION-CENTROID-COL"),
-        request.form.get("REGION-CENTROID-ROW"),
-        request.form.get("REGION-PIXEL-COUNT"),
-        request.form.get("SHORT-LINE-DENSITY-2"),
-        request.form.get("VEDGE-MEAN"),
-        request.form.get("VEDGE-SD"),
-        request.form.get("HEDGE-MEAN"),
-        request.form.get("HEDGE-SD"),
-        request.form.get("INTENSITY-MEAN"),
-        request.form.get("RAWRED-MEAN"),
-        request.form.get("RAWBLUE-MEAN"),
-        request.form.get("RAWGREEN-MEAN"),
-        request.form.get("EXRED-MEAN"),
-        request.form.get("EXBLUE-MEAN"),
-        request.form.get("EXGREEN-MEAN"),
-        request.form.get("VALUE-MEAN"),
-        request.form.get("SATURATION-MEAN"),
-        request.form.get("HUE-MEAN")
+        float(request.form["REGION-CENTROID-COL"]),
+        float(request.form["REGION-CENTROID-ROW"]),
+        # float(request.form["REGION-PIXEL-COUNT"]),
+        float(request.form["SHORT-LINE-DENSITY-5"]),
+        float(request.form["SHORT-LINE-DENSITY-2"]),
+        float(request.form["VEDGE-MEAN"]),
+        float(request.form["VEDGE-SD"]),
+        float(request.form["HEDGE-MEAN"]),
+        float(request.form["HEDGE-SD"]),
+        float(request.form["INTENSITY-MEAN"]),
+        float(request.form["RAWRED-MEAN"]),
+        float(request.form["RAWBLUE-MEAN"]),
+        float(request.form["RAWGREEN-MEAN"]),
+        float(request.form["EXRED-MEAN"]),
+        float(request.form["EXBLUE-MEAN"]),
+        float(request.form["EXGREEN-MEAN"]),
+        float(request.form["VALUE-MEAN"]),
+        float(request.form["SATURATION-MEAN"]),
+        float(request.form["HUE-MEAN"])
     ]
     prediction = predict(input_data)
     return render_template("home.html", prediction_text=prediction)
+  return "Method Not Allowed"
 
-
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
